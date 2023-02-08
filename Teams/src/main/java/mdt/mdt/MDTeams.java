d13a20dd9b3d29ed9db4bdd57e7f773dc40c511d
NexerG <68482683+NexerG@users.noreply.github.com>
2023-02-08 16:43:18 +0200

    Initial commit
    Adds the "decathlon teams" plugin's development thingy

---
+++ Teams/src/main/java/mdt/mdt/MDTeams.java
@@ -0,0 +1,17 @@
+package mdt.mdt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MDTeams extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
