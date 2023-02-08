e039fc7382a70cc898d8d5341b61dcc2c1141ffa
NexerG <68482683+NexerG@users.noreply.github.com>
2023-02-08 16:59:01 +0200

    final setup
    finalized the projects stuff for initial development.
    added a test command

--- Teams/src/main/java/mdt/mdt/MDTeams.java
+++ Teams/src/main/java/mdt/mdt/MDTeams.java
@@ -1,5 +1,6 @@
 package mdt.mdt;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MDTeams extends JavaPlugin {
@@ -7,7 +8,7 @@ public final class MDTeams extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getLogger().info("tavo mama");
     }
 
     @Override
